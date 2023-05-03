ae254bf4ce1377e036fc7f11307f357105ee7da9
geoffrey-diederichs <112950522+geoffrey-diederichs@users.noreply.github.com>
2023-05-03 09:35:43 +0200

    Add files via upload

---
+++ Basic DLL injection/dll.cpp
@@ -0,0 +1,18 @@
+#include <Windows.h>
+#pragma comment(lib, "user32.lib")
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD nReason, LPVOID lpReserved)
+{
+    switch (nReason){
+    case DLL_PROCESS_ATTACH:
+        MessageBox(NULL,"p0wn3d","coucou",MB_OK);
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}
