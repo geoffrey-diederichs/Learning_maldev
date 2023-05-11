68a7b002dd223ffe8896a0efd21ef876665f9f1d
geoffrey-diederichs <geoffrey.diederichs@ynov.com>
2023-05-11 22:04:28 +0200

    deleted modify_path.sh

--- Basic_DLL_injection/modify_path.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Please enter the path to the DLL :"
-read path
-code="char DLL [] = \"${path}\";"
-sed -i "23s/.*/${code}/" launcher.cpp
-echo "launcher.exe has been modified accordingly."
