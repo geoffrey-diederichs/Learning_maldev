b339eaa8fd4edce1c69f6edf56dd38dd09f155cb
geoffrey-diederichs <geoffrey.diederichs@ynov.com>
2023-05-15 19:41:34 +0200

    rolling_xor.py working

---
+++ Rolling_XOR_cipher/rolling_xor.py
@@ -0,0 +1,32 @@
+import sys
+import binascii
+
+def encode(code: bytearray) -> bytearray :
+    encoded = bytearray([code.pop(0)])
+    
+    for i in range(0, len(code)):
+        encoded.append(code[i] ^ encoded[i])
+
+    return encoded
+
+def decode(code: bytearray) -> bytearray:
+    code.reverse()
+    decoded = bytearray()
+
+    for i in range(0, len(code)-1):
+        decoded.append(code[i] ^ code[i+1])
+    decoded.append(code[len(code)-1])
+
+    decoded.reverse()
+    return decoded
+
+
+code = b'\x12\x26\x70'
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "0":
+        result = encode(bytearray(code))
+    elif sys.argv[1] == "1":
+        result = decode(bytearray(code))
+
+print("\\x"+"\\x".join(str(binascii.hexlify(result, '-')).replace("b","").replace("'", "").split('-')))
