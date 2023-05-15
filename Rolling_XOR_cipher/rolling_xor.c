399f696a6e40d5470923eb68103fad7545ab4718
geoffrey-diederichs <geoffrey.diederichs@ynov.com>
2023-05-15 18:02:46 +0200

    rolling_xor.c done

--- Rolling_XOR_cipher/rolling_xor.c
+++ Rolling_XOR_cipher/rolling_xor.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-unsigned char code[] =  "\xdb\x1b\xa5\x12\x6c\x7f\xe1\x38\x4c\x68\x9c\xc4\xf5\x2e";
+int strrev(char *str);
+int encode();
+int decode();
+int print_bytes(char *str);
+
+unsigned char code[] =  "\x12\x26\x70";
+int code_size = sizeof(code);
+
+int main(int argc, char* argv[])
+{
+	if (argc > 0) {
+		if (strcmp(argv[1], "0") == 0)
+			encode();
+		else if (strcmp(argv[1], "1") == 0)
+			decode();
+	}
+	
+	return -1;
+}
 
 int strrev(char *str)
 {
@@ -19,19 +37,15 @@ int strrev(char *str)
 
 int encode()
 {
-	char encoded[sizeof(code)];	
+	char encoded[code_size];	
 	
 	encoded[0] = code[0];
 	int i;
-	for (i = 1; i < sizeof(code)-2; i++)
+	for (i = 1; i < code_size-1; i++)
 		encoded[i] = code[i] ^ encoded[i-1];
-	encoded[i] = code[0] ^ encoded[i-1];
-	encoded[++i] = '\0';
+	encoded[i] = '\0';
 
-	printf("%d\n", sizeof(encoded));
-	for (i = 0; i < sizeof(encoded); i++)
-		printf("\\x%hhx", encoded[i]);
-	printf("\n");
+	print_bytes(encoded);
 
 	return 0;	
 }
@@ -39,30 +53,29 @@ int encode()
 int decode()
 {
 	strrev(code);
-	unsigned char decoded[sizeof(code)];
-	decoded[0] = '\0';
+	unsigned char decoded[code_size];
 
-	int i;
-	for (i = 1; i < sizeof(code)-1; i++)
+	int i, len = strlen(code);
+	for (i = 0; i < len-1; i++) 
 		decoded[i] = code[i] ^ code[i+1];
-	decoded[i] = code [i];
+	decoded[i] = code[i];
+	decoded[++i] = '\0';
 	strrev(decoded);
-	printf("%d\n", sizeof(decoded));                 	
-	for (i = 0; i < sizeof(decoded); i++)
-		printf("\\x%hhx", decoded[i]);
-	printf("\n");                         
+
+	print_bytes(decoded);
 	
 	return 0;
 }
 
-int main(int argc, char* argv[])
+int print_bytes(char *str)
 {
-	if (argc > 1) {
-		if (strcmp(argv[1], "0") == 0)
-			encode();
-		else if (strcmp(argv[1], "1") == 0)
-			decode();
-	}
+	int len = strlen(str);
+	printf("%d bytes\n", len);                 		
+	
+	int i;
+	for (i = 0; i < len; i++)
+		printf("\\x%hhx", str[i]);
+	printf("\n");                         
 
 	return 0;
 }
