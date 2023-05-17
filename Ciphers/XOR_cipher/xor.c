b5cc2080048102ba5f7ca8efab3c737f9c9f3059
geoffrey-diederichs <geoffrey.diederichs@ynov.com>
2023-05-18 01:48:34 +0200

    xor.c working

--- Ciphers/XOR_cipher/xor.c
+++ Ciphers/XOR_cipher/xor.c
@@ -3,8 +3,9 @@
 unsigned char message[] =  "<MESSAGE_TO_REPLACE>";
 int message_size = sizeof(message);
 char key[] = "<KEY_TO_REPLACE>";
+int key_size = sizeof(key);
 
-int print_bytes(char *str, int len)
+int print_result(char *str, int len)
 {
 	printf("%d bytes\n", len);                 		
 	
@@ -12,6 +13,10 @@ int print_bytes(char *str, int len)
 	for (i = 0; i < len; i++)
 		printf("\\x%hhx", str[i]);
 	printf("\n");                         
+	
+	for (i = 0; i < len; i++)
+		printf("%c", str[i]);
+	printf("\n");	
 
 	return 0;
 }
@@ -22,10 +27,10 @@ int main(int argc, char* argv[])
 	
 	int i;
 	for (i = 0; i < message_size-1; i++)
-		encoded[i] = message[i] ^ key[0];
+		encoded[i] = message[i] ^ key[i%(key_size-1)];
 	encoded[i] = '\0';
 
-	print_bytes(encoded, message_size-1);
+	print_result(encoded, message_size-1);
 
 	return 0;
 }
